2c9c0301eba404d62b78774fb281f06dcc9b4030
benthomass <ben.thomas@maticapartners.com>
2026-07-28 16:18:54 +0200

    Add conditional routing to async HITL graph

--- agentic-langgraph/05-human-in-the-loop-memory/core/async.py
+++ agentic-langgraph/05-human-in-the-loop-memory/core/async.py
@@ -4,13 +4,14 @@ load_dotenv()
 
 import operator
 from pathlib import Path
-from typing import Annotated, Any
+from typing import Annotated, Any, Sequence
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 
 
 class State(TypedDict):
     aggregate: Annotated[list, operator.add]
+    which: str
 
 
 class ReturnNodeValue:
@@ -22,25 +23,37 @@ class ReturnNodeValue:
         return {"aggregate": [self.value]}
 
 
+def route_bc_or_cd(state: State) -> Sequence[str]:
+    if state["which"] == "bc":
+        return ["b", "c"]
+    return ["c", "d"]
+
+
+intermediate_nodes = ["b", "c", "d"]
+
 graph_path = Path(__file__).parent.parent / "graph-async.png"
 
 builder = StateGraph(State)
 builder.add_node("a", ReturnNodeValue("I'm in A"))
+builder.add_edge(START, "a")
+
 builder.add_node("b", ReturnNodeValue("I'm in B"))
-builder.add_node("b2", ReturnNodeValue("I'm in B2"))
 builder.add_node("c", ReturnNodeValue("I'm in C"))
 builder.add_node("d", ReturnNodeValue("I'm in D"))
-builder.add_edge(START, "a")
-builder.add_edge("a", "b")
-builder.add_edge("a", "c")
-builder.add_edge("b", "b2")
-builder.add_edge(["b2", "c"], "d")
-builder.add_edge("d", END)
+builder.add_node("e", ReturnNodeValue("I'm in E"))
 
+builder.add_conditional_edges("a", route_bc_or_cd, intermediate_nodes)
+
+for node in intermediate_nodes:
+    builder.add_edge(node, "e")
+
+builder.add_edge("e", END)
 graph = builder.compile()
 graph.get_graph().draw_mermaid_png(output_file_path=str(graph_path))
 
 
 if __name__ == "__main__":
     print("Hello, async world!")
-    graph.invoke({"aggregate": []}, {"configurable": {"thread_id": "foo"}})
+    graph.invoke(
+        {"aggregate": [], "which": "bc"}, {"configurable": {"thread_id": "foo"}}
+    )
